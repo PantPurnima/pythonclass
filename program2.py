4203940e9d222a8d9f427f665e793c6001c2b6a9
purnimapant <purnimapant16@gmail.com>
2017-03-17 21:48:41 +0545

    this is quadratic

---
+++ program2.py
@@ -0,0 +1,7 @@
+a = 1
+b = 7
+c = 3
+d = (b**2 - 4*a*c )** (0.5)
+x1 = (-b + d) / 2 *a
+x2 = (-b - d) /2*a
+print "values of x1 and x2", x1,x2
---
+++ program3.py
@@ -0,0 +1,4 @@
+x1 = raw_input ("x1 = ")
+y1 = raw_input ("y1 = ")
+x2 = raw_input ("x2 = ")
+y2 = raw_input ("y2 = ")
