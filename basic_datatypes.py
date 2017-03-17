e42d7801e71d715bf77a919cbff22aa6789b90ab
purnimapant <purnimapant16@gmail.com>
2017-03-17 12:33:32 +0545

    this is basic datatypes

---
+++ basic_datatypes.py
@@ -0,0 +1,8 @@
+an_integer = 6
+a_floating_point = 17.68
+a_boolean = True
+a_string = "foo"
+print "Integer value =",an_integer
+print "Float value = ",a_floating_point
+print "Boolean value =",a_boolean
+print "string value =",a_string
--- variables1.py
+++ variables1.py
@@ -1,5 +1,5 @@
 first_name = "Purnima"
 last_name = "Pant"
 home_town = "Dhangadhi"
-print("Hi! i am ", first_name, lastn_name,".")
-print("I 'm from",home_town,".")
+print "Hi! i am ", first_name, last_name,"."
+print "I 'm from",home_town,"."
