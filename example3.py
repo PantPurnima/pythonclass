4beaf653efbcdf92ac4bb67477bbf4592e677708
purnimapant <purnimapant16@gmail.com>
2017-03-24 10:19:10 +0545

    elif test

---
+++ example3.py
@@ -0,0 +1,12 @@
+word=raw_input("Word:")
+if word =="foo":
+	print 'great!'
+	print 'you entered "foo"'
+elif word== "Bar":
+	print 'Hey,thats a bar :)'
+elif word == "zazz":
+	print'aww i love zazzz'
+else:
+	print "hey!"
+	print "that was something else"
+print "good bye!"
